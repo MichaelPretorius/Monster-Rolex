be216c3f6817443b37b0e41fdba8e3e09ec0359d
Michael Pretorius <michaelpretorius96@gmail.com>
2019-09-05 00:02:31 +0200

    Updates

---
+++ precache-manifest.f6153fdba420fb1e85e199b0e2e5f20d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8581467c11a5bf408bcef782281b1c5d",
+    "url": "/Monster-Rolex/index.html"
+  },
+  {
+    "revision": "bd578a3f732b39e51ce4",
+    "url": "/Monster-Rolex/static/css/main.0446c410.chunk.css"
+  },
+  {
+    "revision": "53aecd42cf3435732362",
+    "url": "/Monster-Rolex/static/js/2.4b7b5c74.chunk.js"
+  },
+  {
+    "revision": "bd578a3f732b39e51ce4",
+    "url": "/Monster-Rolex/static/js/main.c4404bd9.chunk.js"
+  },
+  {
+    "revision": "c673aa5f3c87a48c0714",
+    "url": "/Monster-Rolex/static/js/runtime~main.db565a43.js"
+  }
+]);
